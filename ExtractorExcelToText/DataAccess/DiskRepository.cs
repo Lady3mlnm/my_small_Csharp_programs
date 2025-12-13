1640020536dd5d9cfc3109bcc84e52dd8f9e8466
Malvina Pushkova <lady3mlnm@gmail.com>
2025-12-13 19:03:07 +0400

    ExtractorExcelToText v0.2.3
    Added option to define range of rows consisting of several parts, like "2:4, 5, 10:15"

--- ExtractorExcelToText/DataAccess/DiskRepository.cs
+++ ExtractorExcelToText/DataAccess/DiskRepository.cs
@@ -1,6 +1,9 @@
 ﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
 using ExtractorExcelToText.DataStructures;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ExtractorExcelToText.DataAccess;
 
@@ -19,47 +22,57 @@ internal class DiskRepository : IRepository
     {
         var worksheet = workbook.Worksheet(sheetName);
 
-        var rowBeginEndPositions = rowRange.Split(":");
-        string stRangeBegin = rowBeginEndPositions[0];
-        string stRangeEnd = rowBeginEndPositions[1];
-
-        IEnumerable<int> ieIds = (columnPositions == "auto")
-            ? Enumerable.Range(1, int.Parse(stRangeEnd) - int.Parse(stRangeBegin) + 1)
-            : worksheet.Cells($"{columnPositions}{stRangeBegin}:{columnPositions}{stRangeEnd}")
-                       .Select(cell => int.Parse(cell.Value.ToString()));
-
-        var ieTexts = worksheet.Cells($"{columnTexts}{stRangeBegin}:{columnTexts}{stRangeEnd}")
+        string pattern = @"\d+";
+        string searchTexts = Regex.Replace(rowRange, pattern, columnTexts + "$&");   //= Regex.Replace(rowRange, pattern, m => columnTexts + m.Value);
+        var ieTexts = worksheet.Cells(searchTexts)
                                .Select(cell => cell.Value.ToString());
 
-        return ieIds.Zip(ieTexts, (id, st) => new Record(id, st))
-                    .Where(record => record.Text != cellIgnoringMark)
-                    .OrderBy(record => record.Position);
+        if(columnPositions == "auto") {
+            return ieTexts.Select((st, index) => new Record(index+1, st))
+                          .Where(record => record.Text != cellIgnoringMark)
+                          .OrderBy(record => record.Position);
+        } else {
+            string searchPositions = Regex.Replace(rowRange, pattern, columnPositions + "$&");
+
+            IEnumerable<int> iePos = worksheet.Cells(searchPositions)
+                                              .Select(cell => int.Parse(cell.Value.ToString()));
+
+            return ieTexts.Zip(iePos, (st, pos) => new Record(pos, st))
+                          .Where(record => record.Text != cellIgnoringMark)
+                          .OrderBy(record => record.Position);
+        }
     }
 
 
-    public IOrderedEnumerable<Record> ReadExcelTwoColumnsCombined(ref XLWorkbook workbook, string sheetName, string columnPositions, string columnTexts, string columnOverlay, string rowRange, string? cellIgnoringMark)
+    public IOrderedEnumerable<Record> ReadExcelTwoColumnsCombined(ref XLWorkbook workbook, string sheetName, string columnPositions, string columnTexts, string columnOverlaps, string rowRange, string? cellIgnoringMark)
     {
         var worksheet = workbook.Worksheet(sheetName);
 
-        var rowBeginEndPositions = rowRange.Split(":");
-        string stRangeBegin = rowBeginEndPositions[0];
-        string stRangeEnd = rowBeginEndPositions[1];
-
-        IEnumerable<int> ieIds = (columnPositions == "auto")
-            ? Enumerable.Range(1, int.Parse(stRangeEnd) - int.Parse(stRangeBegin) + 1)
-            : worksheet.Cells($"{columnPositions}{stRangeBegin}:{columnPositions}{stRangeEnd}")
-                       .Select(cell => int.Parse(cell.Value.ToString()));
-
-        var ieTexts = worksheet.Cells($"{columnTexts}{stRangeBegin}:{columnTexts}{stRangeEnd}")
+        string pattern = @"\d+";
+        string searchTexts = Regex.Replace(rowRange, pattern, columnTexts + "$&");   //= Regex.Replace(rowRange, pattern, m => columnTexts + m.Value);
+        var ieTexts = worksheet.Cells(searchTexts)
                                .Select(cell => cell.Value.ToString());
 
-        var ieOverlaps = worksheet.Cells($"{columnOverlay}{stRangeBegin}:{columnOverlay}{stRangeEnd}")
+        string searchOverlaps = Regex.Replace(rowRange, pattern, columnOverlaps + "$&");
+        var ieOverlaps = worksheet.Cells(searchOverlaps)
                                   .Select(cell => cell.Value.ToString());
 
-        return ieTexts.Zip(ieOverlaps, (stOriginal, stOverlap) => (stOverlap == cellIgnoringMark) ? stOriginal : stOverlap)
-                      .Zip(ieIds, (st, id) => new Record(id, st))
-                      .Where(record => record.Text != cellIgnoringMark)
-                      .OrderBy(record => record.Position);
+        if(columnPositions == "auto") {
+            return ieTexts.Zip(ieOverlaps, (stOriginal, stOverlap) => (stOverlap == cellIgnoringMark) ? stOriginal : stOverlap)
+                          .Select((st, index) => new Record(index + 1, st))
+                          .Where(record => record.Text != cellIgnoringMark)
+                          .OrderBy(record => record.Position);
+        } else {
+            string searchPositions = Regex.Replace(rowRange, pattern, columnPositions + "$&");
+
+            IEnumerable<int> iePos = worksheet.Cells(searchPositions)
+                                              .Select(cell => int.Parse(cell.Value.ToString()));
+
+            return ieTexts.Zip(ieOverlaps, (stOriginal, stOverlap) => (stOverlap == cellIgnoringMark) ? stOriginal : stOverlap)
+                          .Zip(iePos, (st, pos) => new Record(pos, st))
+                          .Where(record => record.Text != cellIgnoringMark)
+                          .OrderBy(record => record.Position);
+        }
     }
 
 
